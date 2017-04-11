5cae61e6a16b2b63faa862b6dd451b3028937f66
NowThenSun <harrywangwork@gmail.com>
2017-04-11 22:22:21 +0100

    Added possibility of different dt_fixed for RK4 in phot_flow 1D ODE comparison plots

--- phot_flow_1D_ODE_comparison.py
+++ phot_flow_1D_ODE_comparison.py
@@ -23,10 +23,10 @@ TIME = fh.variables['TIME'][:]
 fh.close()
 
 #~~~~~ 1D PLOTTING PARAMETERS ~~~~~
-y = 5000                                # fixed y-value the FTLE is evaluated at
+y = 2000                                # fixed y-value the FTLE is evaluated at
 x0 = X[0]                                  # initial x value
 x1 = 5500                                # final x value
-res = np.array([200,500]) # Resolutions tested at (number of grid points between x0 and x1)               0.2/100 ~ 1000x500 res
+res = np.array([20]) # Resolutions tested at (number of grid points between x0 and x1)               0.2/100 ~ 1000x500 res
 grid_spacing = (x1-x0)/res
 #True resolution calculation
 # Main parameter (need to make sure this matches with grid_lim_step used in data calculation)
@@ -36,13 +36,14 @@ actual_res = (X_max-X_min)*res/(x1-x0)
 
 # other parameters
 aux_grid_spacing = grid_spacing*0.08
-rkf45_error_tol = 1*10**2
+rkf45_error_tol = 1*10**-2
 dp45_error_tol = rkf45_error_tol
 t_0 = TIME[-1]
 int_time = -14400
 dt_min = np.sign(int_time)*10
 dt_max = np.sign(int_time)*300
-dt_fixed = np.sign(int_time)*300      # timestep used for RK4 integration
+# dt_fixed = np.sign(int_time)*50      # timestep used for RK4 integration
+dt_fixed_array = [-25,-75,-200,-500]        # array of timesteps used for RK4 integration
 
 
 # Bools of which methods are compared
@@ -53,7 +54,7 @@ DP45 = True
 no_methods = [RK4,RKF45,DP45].count(True)
 #list to put in final data of calculated FTLE fields
 
-FTLE_list_RK4 = []
+# FTLE_list_RK4 = []
 FTLE_list_RKF45 = []
 FTLE_list_DP45 = []
 fig = plt.figure()
@@ -73,19 +74,20 @@ for j in xrange(len(res)):
     print np.shape(a_grid)
 
     if RK4 == True:
-        # Now use rk4 scheme for
-        final_pos = fn.rk4_loop(derivs=interp.regular_grid_interpolator_fn(U, V, X, Y, TIME)[1], aux_grid=a_grid, t_0=t_0, int_time=int_time, dt=dt_fixed)
+        for k in xrange(len(dt_fixed_array)):
+            # Now use rk4 scheme for
+            final_pos = fn.rk4_loop(derivs=interp.regular_grid_interpolator_fn(U, V, X, Y, TIME)[1], aux_grid=a_grid, t_0=t_0, int_time=int_time, dt=dt_fixed_array[k])
 
-        jac = fn.jacobian_matrix_aux(final_pos,aux_grid_spacing=aux_grid_spacing[j])
-        cgst = fn.cauchy_green_tensor(jac)
-        ev = np.linalg.eigvalsh(cgst)
-        ev_max = np.amax(ev,-1)
-        ftle = np.log(ev_max)/(2.*np.abs(int_time))
-        # Append FTLE data to FTLE_list
-        FTLE_list_RK4.append(ftle)
+            jac = fn.jacobian_matrix_aux(final_pos,aux_grid_spacing=aux_grid_spacing[j])
+            cgst = fn.cauchy_green_tensor(jac)
+            ev = np.linalg.eigvalsh(cgst)
+            ev_max = np.amax(ev,-1)
+            ftle = np.log(ev_max)/(2.*np.abs(int_time))
+            # Append FTLE data to FTLE_list
+            # FTLE_list_RK4.append(ftle)
 
-        plt.plot(np.linspace(x0,x1,res[j]),FTLE_list_RK4[j][0],'-', alpha=line_alpha)
-        legends.append('RK4 nx%i dt = %.2f' %(actual_res[j], dt_fixed))
+            plt.plot(np.linspace(x0,x1,res[j]),ftle[0],'-', alpha=line_alpha)
+            legends.append('RK4 nx%i dt = %.2f' %(actual_res[j], dt_fixed_array[k]))
 
     if RKF45 == True:
         # Use rkf45 integration scheme
