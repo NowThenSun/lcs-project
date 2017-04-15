f9ba23fedc31283a644f4f12d9e480b1b696b6f0
NowThenSun <harrywangwork@gmail.com>
2017-04-15 11:46:35 +0100

    Updated gyre_1D_plots to include dopri54 method and updated to phot_flow_1D's level

--- gyre_1D_plots.py
+++ gyre_1D_plots.py
@@ -8,7 +8,7 @@ import numpy as np
 import general_functions as fn
 import double_gyre_functions as dg
 import matplotlib.pyplot as plt
-
+import ODE_solvers as ODE
 
 
 #~~~~~ 1D PLOTTING PARAMETERS ~~~~~
@@ -17,59 +17,100 @@ x0 = 1.0                                # first x value
 x1 = 1.2                                # final x value
 res = np.array([50,300,500]) # Resolutions tested at (number of grid spacing between x0 and x1)               0.2/100 ~ 1000x500 res
 actual_res = res*2./(x1-x0)     # True resolution on a full [0,2]x[0,1] double gyre domain
+grid_spacing = (x1-x0)/res
+
 # double gyre parameters - Note: dg.gyre_global_params's global parameters have dg. in front of them so primarily exist within the dg module (although can still be called upon here with e.g. dg.amplitude_g)
 dg_params = dg.gyre_global_params(amplitude=0.1, epsilon=0.1, omega=2*np.pi/10.)
 # other parameters
-aux_grid_spacing = 1*10**-5
-adaptive_error_tol = 1*10**-3
+aux_grid_spacing = 0.08*grid_spacing
+rkf45_error_tol = 1*10**-2
+dp45_error_tol = rkf45_error_tol
 t_0 = 2.
 int_time = 10.
 dt_min = np.sign(int_time)*1*10**-3
 dt_max = 1.
-dt_fixed = 0.5      # timestep used for RK4 integration
+# dt_fixed = 0.5      # timestep used for RK4 integration
+dt_fixed_array = [0.5]        # array of timesteps used for RK4 integration
 
+# Bools of which methods are compared
+Truez=False
+RK4 = True
+RKF45 = True
+DP45 = True
+no_methods = [RK4,RKF45,DP45].count(True)
 #list to put in final data of calculated FTLE fields
-FTLE_list = []
-
-for j in xrange(len(res)):
-    print "RESOLUTION =", actual_res[j]
-    # Generate initial grid and aux. grid
-    initial_grid = np.array(np.meshgrid(np.linspace(x0,x1,res[j]),y,indexing='xy'))  # 2*1*res[j] grid
-    a_grid = fn.generate_auxiliary_grid(initial_grid, aux_grid_spacing=aux_grid_spacing)
-    print np.shape(a_grid)
-    # Use rkf45 integration scheme
-    final_pos = fn.rkf45_loop(derivs=dg.analytic_velocity, aux_grid=a_grid, adaptive_error_tol=adaptive_error_tol, t_0=t_0, int_time=int_time, dt_min=dt_min, dt_max = dt_max)
-    jac = fn.jacobian_matrix_aux(final_pos,aux_grid_spacing=aux_grid_spacing)
-    cgst = fn.cauchy_green_tensor(jac)
-    ev = np.linalg.eigvalsh(cgst)
-    ev_max = np.amax(ev,-1)
-    ftle = np.log(ev_max)/(2.*np.abs(int_time))
-    # Append FTLE data to FTLE_list
-    FTLE_list.append(ftle)
-
-    # Now use rk4 scheme for
-    final_pos = fn.rk4_loop(derivs=dg.analytic_velocity, aux_grid=a_grid, t_0=t_0, int_time=int_time, dt=dt_fixed)
-    jac = fn.jacobian_matrix_aux(final_pos,aux_grid_spacing=aux_grid_spacing)
-    cgst = fn.cauchy_green_tensor(jac)
-    ev = np.linalg.eigvalsh(cgst)
-    ev_max = np.amax(ev,-1)
-    ftle = np.log(ev_max)/(2.*np.abs(int_time))
-    # Append FTLE data to FTLE_list
-    FTLE_list.append(ftle)
-
-#Plotting code
-print len(FTLE_list)
 
 fig = plt.figure()
 ax = plt.axes()
 ax.set_xlabel('x')
 ax.set_ylabel('FTLE')
 legends = []
+line_alpha = 0.66
+dp_line_alpha = 0.7
+
 for j in xrange(len(res)):
-    plt.plot(np.linspace(x0,x1,res[j]),FTLE_list[2*j][0], '--')
-    legends.append('RKF45 %ix%i' %(actual_res[j],actual_res[j]/2))
-    plt.plot(np.linspace(x0,x1,res[j]),FTLE_list[2*j+1][0], '-')
-    legends.append('RK4 %ix%i dt = %.2f' %(actual_res[j],actual_res[j]/2, dt_fixed))
+    print "Grid spacing =", grid_spacing[j]
+    # Generate initial grid and aux. grid
+    initial_grid = np.array(np.meshgrid(np.linspace(x0,x1,res[j]),y,indexing='xy'))  # 2*1*res[j] grid
+    print np.shape(initial_grid)
+    a_grid = fn.generate_auxiliary_grid(initial_grid, aux_grid_spacing=aux_grid_spacing[j])
+    print np.shape(a_grid)
+
+    if RK4 == True:
+        for k in xrange(len(dt_fixed_array)):
+            # Now use rk4 scheme for
+            final_pos = fn.rk4_loop(derivs=dg.analytic_velocity, aux_grid=a_grid, t_0=t_0, int_time=int_time, dt=dt_fixed_array[k])
+
+            jac = fn.jacobian_matrix_aux(final_pos,aux_grid_spacing=aux_grid_spacing[j])
+            cgst = fn.cauchy_green_tensor(jac)
+            ev = np.linalg.eigvalsh(cgst)
+            ev_max = np.amax(ev,-1)
+            ftle = np.log(ev_max)/(2.*np.abs(int_time))
+            # Append FTLE data to FTLE_list
+            # FTLE_list_RK4.append(ftle)
+
+            plt.plot(np.linspace(x0,x1,res[j]),ftle[0],'-', alpha=line_alpha)
+            legends.append('RK4 nx%i dt = %.2f' %(actual_res[j], dt_fixed_array[k]))
+
+    if RKF45 == True:
+        # Use rkf45 integration scheme
+        final_positions = ODE.rkf45_loop(
+        derivs=dg.analytic_velocity, aux_grid=a_grid,
+        t_0=t_0,
+        int_time=int_time, dt_min=dt_min, dt_max = dt_max,maxiters = 1000, atol=rkf45_error_tol, rtol =rkf45_error_tol)
+
+        jac = fn.jacobian_matrix_aux(final_positions,aux_grid_spacing=aux_grid_spacing[j])
+        cgst = fn.cauchy_green_tensor(jac)
+        ev = np.linalg.eigvalsh(cgst)
+        ev_max = np.amax(ev,-1)
+        ftle = np.log(ev_max)/(2.*np.abs(int_time))
+        # Append FTLE data to FTLE_list
+        # FTLE_list_RKF45.append(ftle)
+        plt.plot(np.linspace(x0,x1,res[j]),ftle[0],'--', alpha=line_alpha)
+        legends.append('RKF45 nx%i etol = %r' %(actual_res[j], rkf45_error_tol))
+
+    if DP45 == True:
+        #DP45 scheme
+        final_positions = ODE.dp45_loop(
+            derivs=dg.analytic_velocity, aux_grid=a_grid,
+            t_0=t_0,
+            int_time=int_time, dt_min=dt_min, dt_max = dt_max,maxiters = 1000, atol=dp45_error_tol, rtol =dp45_error_tol)
+
+        jac = fn.jacobian_matrix_aux(final_positions,aux_grid_spacing=aux_grid_spacing[j])
+        cgst = fn.cauchy_green_tensor(jac)
+        ev = np.linalg.eigvalsh(cgst)
+        ev_max = np.amax(ev,-1)
+        ftle = np.log(ev_max)/(2.*np.abs(int_time))
+        # FTLE_list_DP45.append(ftle)
+        plt.plot(np.linspace(x0,x1,res[j]),ftle[0],':', alpha=dp_line_alpha, marker='x')
+        legends.append('DOPRI54 nx%i etol = %r' %(actual_res[j], dp45_error_tol))
+
+
+
+#
 plt.legend(legends)
+ax.text(0.8,1.02,'y = %r' %y, transform=ax.transAxes)
+ax.text(0.2,1.02,'t_0= %r' %(t_0), transform=ax.transAxes)
+
 #plt.savefig('testfig23.pdf',transparent=True)
 plt.show()
