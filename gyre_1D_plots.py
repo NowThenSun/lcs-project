b5be2cd810a7b525a71c90d41811c0e590d97f6a
NowThenSun <harrywangwork@gmail.com>
2017-04-19 19:41:13 +0100

    Minor bugfix, 1D plots (can now use negative integration times)

--- gyre_1D_plots.py
+++ gyre_1D_plots.py
@@ -16,7 +16,7 @@ import matplotlib
 y = 0.3                                  # fixed y-value the FTLE is evaluated at
 x0 = 1.0                                # first x value
 x1 = 1.2                                # final x value
-res = np.array([50,100,500]) # Resolutions tested at (number of grid spacing between x0 and x1)               0.2/100 ~ 1000x500 res
+res = np.array([100]) # Resolutions tested at (number of grid spacing between x0 and x1)               0.2/100 ~ 1000x500 res
 actual_res = res*2./(x1-x0)     # True resolution on a full [0,2]x[0,1] double gyre domain
 grid_spacing = (x1-x0)/res
 
@@ -29,14 +29,14 @@ dp45_error_tol = rkf45_error_tol
 t_0 = 2.
 int_time = 10.
 dt_min = np.sign(int_time)*1*10**-3
-dt_max = 1.
+dt_max = np.sign(int_time)*1.
 # dt_fixed = 0.5      # timestep used for RK4 integration
-dt_fixed_array = [0.5]        # array of timesteps used for RK4 integration
+dt_fixed_array = [np.sign(int_time)*0.5]        # array of timesteps used for RK4 integration
 
 # Bools of which methods are compared
 Truez=False
-RK4 = Truez
-RKF45 = Truez
+RK4 = True
+RKF45 = True
 DP45 = True
 no_methods = [RK4,RKF45,DP45].count(True)
 #list to put in final data of calculated FTLE fields
@@ -112,26 +112,26 @@ for j in xrange(len(res)):
         FTLE_list_DP45.append(ftle)
         # marks = ['x','o',None]
         # ls = [':',':','-']
-        plt.plot(np.linspace(x0,x1,res[j]),ftle[0],[:], alpha=dp_line_alpha, marker='x')
-        legends.append('DOPRI54: $\mathregular{h_{grid}}$=%.1e  ' %(grid_spacing[j])) # , $\mathregular{\tau}$ =%.0e   , dp45_error_tol
+        plt.plot(np.linspace(x0,x1,res[j]),ftle[0],':', alpha=dp_line_alpha, marker='x', ms = 3)
+        legends.append(r'DOPRI54: $\mathregular{h_{grid}}$=%.1e  , $\mathregular{\tau}$ =%.0e ' %(grid_spacing[j] , dp45_error_tol)) #
 
-plt.legend(legends, loc=[0.02,0.81])
+plt.legend(legends) #, loc=[0.02,0.81]
 
-
-axins = zoomed_inset_axes(ax, 2.5  , loc=1) # zoom = 6
-for j in xrange(len(res)):
-    axins.plot(np.linspace(x0,x1,res[j]),FTLE_list_DP45[j][0],':', alpha=dp_line_alpha, marker='x')
 #
-# sub region of the original image
-X1, X2, Y1, Y2 = 1.063, 1.092, 0.335, 0.575
-axins.set_xlim(X1, X2)
-axins.set_ylim(Y1, Y2)
-
-plt.xticks(visible=False)
-plt.yticks(visible=False)
-# draw a bbox of the region of the inset axes in the parent axes and
-# connecting lines between the bbox and the inset axes area
-mark_inset(ax, axins, loc1=2, loc2=3, fc="none", ec="0.5")
+# axins = zoomed_inset_axes(ax, 2.5  , loc=1) # zoom = 6
+# for j in xrange(len(res)):
+#     axins.plot(np.linspace(x0,x1,res[j]),FTLE_list_DP45[j][0],':', alpha=dp_line_alpha, marker='x')
+# #
+# # sub region of the original image
+# X1, X2, Y1, Y2 = 1.063, 1.092, 0.335, 0.575
+# axins.set_xlim(X1, X2)
+# axins.set_ylim(Y1, Y2)
+#
+# plt.xticks(visible=False)
+# plt.yticks(visible=False)
+# # draw a bbox of the region of the inset axes in the parent axes and
+# # connecting lines between the bbox and the inset axes area
+# mark_inset(ax, axins, loc1=2, loc2=3, fc="none", ec="0.5")
 
 
 
