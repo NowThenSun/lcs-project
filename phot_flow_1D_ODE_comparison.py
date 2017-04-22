30c6e98b4ba26f750bb902c8fcff30b12958bf7a
NowThenSun <harrywangwork@gmail.com>
2017-04-23 00:32:41 +0100

    Removed unnecessary labels for final plots

--- phot_flow_1D_ODE_comparison.py
+++ phot_flow_1D_ODE_comparison.py
@@ -29,13 +29,13 @@ print time.strftime('%d-%b-%Y %H:%M GMT', time.gmtime(INIT_TIME))
 REF_TIME = INIT_TIME - TIME[0]
 #~~~~~ 1D PLOTTING PARAMETERS ~~~~~
 y = 2000                                # fixed y-value the FTLE is evaluated at
-x0 = X[0]+2000                                 # initial x value
+x0 = X[0]+2000                                # initial x value
 x1 = 4000                                # final x value
-res = np.array([50,100,200]) # Resolutions tested at (number of grid points between x0 and x1)               0.2/100 ~ 1000x500 res
+res = np.array([400,20]) # Resolutions tested at (number of grid points between x0 and x1)               0.2/100 ~ 1000x500 res
 grid_spacing = (x1-x0)/res
 #True resolution calculation
 # Main parameter (need to make sure this matches with grid_lim_step used in data calculation)
-grid_lim_step = 2
+grid_lim_step = 4
 X_min,X_max, Y_min, Y_max = (X[grid_lim_step],X[-grid_lim_step-1],Y[grid_lim_step],Y[-grid_lim_step-1])  # Limit initial grid size
 actual_res = (X_max-X_min)*res/(x1-x0)
 
@@ -46,7 +46,7 @@ dp45_error_tol = rkf45_error_tol
 t_0 = TIME[-1]
 int_time = -14400*3
 dt_min = np.sign(int_time)*1
-dt_max = np.sign(int_time)*100
+dt_max = np.sign(int_time)*200
 # dt_fixed = np.sign(int_time)*50      # timestep used for RK4 integration
 dt_fixed_array = [-100]        # array of timesteps used for RK4 integration
 
@@ -54,7 +54,7 @@ dt_fixed_array = [-100]        # array of timesteps used for RK4 integration
 # Bools of which methods are compared
 Truez=False
 RK4 = True
-RKF45 = Truez
+RKF45 = True
 DP45 = True
 no_methods = [RK4,RKF45,DP45].count(True)
 #list to put in final data of calculated FTLE fields
@@ -62,13 +62,13 @@ no_methods = [RK4,RKF45,DP45].count(True)
 # FTLE_list_RK4 = []
 FTLE_list_RKF45 = []
 FTLE_list_DP45 = []
-fig = plt.figure()
+fig = plt.figure(figsize=(8,6))
 ax = plt.axes()
 ax.set_xlabel('x (%s)' %lenunits)
-ax.set_ylabel('FTLE')
+ax.set_ylabel(r'FTLE (s$^{-1}$)')
 legends = []
-line_alpha = 0.66
-dp_line_alpha = 0.7
+line_alpha = 0.6
+dp_line_alpha = 0.6
 
 for j in xrange(len(res)):
     print "Grid spacing =", grid_spacing[j]
@@ -92,7 +92,7 @@ for j in xrange(len(res)):
             # FTLE_list_RK4.append(ftle)
 
             plt.plot(np.linspace(x0,x1,res[j]),ftle[0],'-', alpha=line_alpha)
-            legends.append(r'RK4: $\mathregular{h_\mathrm{grid}}$=%d  , $\mathregular{\Delta t}$ = %.1f' %(grid_spacing[j], dt_fixed_array[k]))
+            legends.append(r'RK4: $\mathregular{h_\mathrm{grid}}$=%d km , $\mathregular{\Delta t} $ = %d s' %(grid_spacing[j], dt_fixed_array[k]))
 
     if RKF45 == True:
         # Use rkf45 integration scheme
@@ -109,7 +109,7 @@ for j in xrange(len(res)):
         # Append FTLE data to FTLE_list
         FTLE_list_RKF45.append(ftle)
         plt.plot(np.linspace(x0,x1,res[j]),FTLE_list_RKF45[j][0],'--', alpha=line_alpha)
-        legends.append(r'RKF45: $\mathregular{h_{grid}}$=%d  , $\mathregular{\tau}$ =%.0e' %(grid_spacing[j], rkf45_error_tol))
+        legends.append(r'RKF45: $\mathregular{h_{grid}}$=%d km , $\mathregular{\tau}$ =%.0e' %(grid_spacing[j], rkf45_error_tol))
 
     if DP45 == True:
         #DP45 scheme
@@ -125,15 +125,15 @@ for j in xrange(len(res)):
         ftle = np.log(ev_max)/(2.*np.abs(int_time))
         FTLE_list_DP45.append(ftle)
         plt.plot(np.linspace(x0,x1,res[j]),FTLE_list_DP45[j][0],':', alpha=dp_line_alpha, marker='x')
-        legends.append(r'DOPRI54: $\mathregular{h_{grid}}$=%d  , $\mathregular{\tau}$ =%.0e ' %(grid_spacing[j] , dp45_error_tol))
+        legends.append(r'DOPRI54: $\mathregular{h_{grid}}$=%d km , $\mathregular{\tau}$ =%.0e ' %(grid_spacing[j] , dp45_error_tol))
 
 
 
 #
-plt.legend(legends)
-ax.text(0.8,1.02,'y = %r %s' %(y,lenunits), transform=ax.transAxes)
-ax.text(0.0,1.02,'t_0= %s' %time.strftime('%d-%b-%Y %H:%M UT', time.gmtime((t_0 + REF_TIME))), transform=ax.transAxes)
-ax.text(0.55,1.02, 'T = %+.1f hrs' %(int_time/3600.) , transform=ax.transAxes)
+# plt.legend(legends)
+# ax.text(0.8,1.02,'y = %r %s' %(y,lenunits), transform=ax.transAxes)
+# ax.text(0.0,1.02,'t_0= %s' %time.strftime('%d-%b-%Y %H:%M UT', time.gmtime((t_0 + REF_TIME))), transform=ax.transAxes)
+# ax.text(0.55,1.02, 'T = %+.1f hrs' %(int_time/3600.) , transform=ax.transAxes)
 #plt.savefig('testfig23.pdf',transparent=True)
 print actual_res
 plt.show()
